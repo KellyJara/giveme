63d373db38fb1de145466b55aa42e4ccff2b2d99
KellyJara <vaneorozco24@hotmail.com>
2025-03-28 20:22:35 +0100

    create header component

--- angular-basics/my-first-project/src/app/app.component.ts
+++ angular-basics/my-first-project/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MyComponentComponent } from "./my-component/my-component.component";
-import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MyComponentComponent, HeaderComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'my-first-project';
 }
